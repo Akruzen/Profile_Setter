8098cd431e2b499e1ed15889ce11a18e97197bc8
Akruzen <1omkarphadke@gmail.com>
2022-07-10 12:04:00 +0530

    Added Material You support

--- app/src/main/java/com/akruzen/profile_setter/MainActivity.java
+++ app/src/main/java/com/akruzen/profile_setter/MainActivity.java
@@ -2,16 +2,42 @@ package com.akruzen.profile_setter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void goToRepoTapped (View view) {
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Akruzen/Profile_Setter"));
         startActivity(browserIntent);
+    }
+
+    private void getPermission () {
+        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (!notificationManager.isNotificationPolicyAccessGranted()) {
+            Intent intent = new Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+            startActivity(intent);
+        }
+    }
+
+    private boolean permissionGranted() {
+        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        return notificationManager.isNotificationPolicyAccessGranted();
+    }
+
+    public void checkPermissionTapped (View view) {
+        if (!permissionGranted()) {
+            Toast.makeText(this, "Please select Pro Set here and allow DnD permission.", Toast.LENGTH_SHORT).show();
+            getPermission();
+        } else {
+            Toast.makeText(this, "Permission already granted.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
